1fa6b271d1584bf02bc2bab1ba000d52d3fe4907
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2024-12-13 10:31:41 +0530

    yarn update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="border h-screen border-red-200">
+    <div className="border mt-16 h-screen border-red-900">
       <h1 className="text-3xl text-blue-500 lora tracking-wide">Askora</h1>
     </div>
   );
