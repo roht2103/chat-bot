18e548ec8728be05ee6d5ed99ab12da4280283e0
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2024-12-13 13:39:54 +0530

    added routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./App.css";
 import { Nav } from "./components/Nav.jsx";
 import { Header } from "./components/Header.jsx";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className=" h-screen overflow-auto">
-      <Nav />
-      <Header />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className=" h-screen overflow-auto">
+              <Nav />
+              <Header />
+              <Routes>
+                <Route path="/askora-chat-bot" />
+              </Routes>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { Button, Flex } from "antd";
+import { Navigate, useNavigate } from "react-router-dom";
 export const Header = () => {
+  const navigate = useNavigate();
   return (
     <div className="h-full flex items-center justify-center flex-col">
       <p className="lg:text-[10rem] text-8xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 lora tracking-wide animate-gradient">
@@ -9,7 +11,11 @@ export const Header = () => {
         Where curiosity meets clarity
       </p>
       <Flex className="gap-3 mt-3" wrap>
-        <Button type="primary" className="text-lg">
+        <Button
+          type="primary"
+          className="text-lg"
+          onClick={() => navigate("/askora-chat-bot")}
+        >
           Start a chat
         </Button>
         <Button className="text-lg">Set Reminders</Button>
