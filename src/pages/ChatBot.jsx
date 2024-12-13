8458b82c784c56d025eaf4697cd92a62f1e2a887
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2024-12-13 21:13:03 +0530

    handeled gemini api request

--- src/pages/ChatBot.jsx
+++ src/pages/ChatBot.jsx
@@ -1,13 +1,17 @@
 import { ChatBotNav } from "../components/ChatBotNav";
 import { InputComponent } from "../components/InputComponent";
 import { useState } from "react";
-
+import { GoogleGenerativeAI } from "@google/generative-ai";
 export const ChatBot = () => {
   const [text, setText] = useState();
-  const [response, setResponse] = useState("");
-
-  const getResponse = () => {
-    alert("hello");
+  const [chats, setChats] = useState({});
+  const API_KEY = "AIzaSyA2xOaQUtb_teLDoGwC9Z3OvPtA10vAVb8";
+  const getResponse = async (query) => {
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const result = await model.generateContent(query);
+    setChats({ req: query, res: result.response.text() });
+    console.log(result.response.text());
   };
   return (
     <div className="h-screen flex flex-col relative">
